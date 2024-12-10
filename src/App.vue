19d81b1b552ac34f84234109132428758209a197
miyaji-yusei-fixer <miyaji.yusei@fixer.co.jp>
2024-12-10 20:33:23 +0900

    :sparkles: カウントダウンでブラウザを閉じる

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   {{ displayMessage }}
+  <br />
+  {{ closedMessage }}
 </template>
 
 <script lang="ts">
@@ -19,35 +21,55 @@ export default defineComponent({
   name: 'App',
   setup() {
     const displayMessage = ref("");
+    const closedMessage = ref("");
     const isInClient = ref<boolean | "NOT_INITIALIZED">("NOT_INITIALIZED");
     const liffState = reactive<LiffState>({
       profile: undefined,
     });
+    const delay = 5000; // 5秒後に閉じる
+    const countdownInterval = ref<ReturnType<typeof setInterval> | null>(null); // タイマーのIDを保持するための変数
+    const countdownTime = ref(delay / 1000); // カウントダウンの時間（秒）
 
     const getProfile = async () => {
       const liffId = process.env.VUE_APP_LIFF_ID || "";
-      isInClient.value = await liff.isInClient()
+      isInClient.value = await liff.isInClient();
       if (isInClient.value) {
         try {
           displayMessage.value = `LIFF ID：${liffId}`;
           await liff.init({ liffId: liffId });
           const profile = await liff.getProfile();
           liffState.profile = profile;
-          displayMessage.value = liffState.profile.userId
+          displayMessage.value = liffState.profile.userId;
         } catch {
-          displayMessage.value = "エラーが発生しました。"
+          displayMessage.value = "エラーが発生しました。";
         }
       } else {
         displayMessage.value = "LIFF IDが設定されていないか、LINEブラウザで開かれていません";
       }
     };
 
+    const startTimer = () => {
+      closedMessage.value = `${countdownTime.value}秒後にブラウザを閉じます`;
+
+      countdownInterval.value = setInterval(() => {
+        countdownTime.value -= 1;
+        if (countdownTime.value <= 0) {
+          clearInterval(countdownInterval.value!); // インターバルをクリア
+          liff.closeWindow(); // ブラウザを閉じる
+        } else {
+          closedMessage.value = `${countdownTime.value}秒後にブラウザを閉じます`;
+        }
+      }, 1000); // 1秒ごとに更新
+    };
+
     onMounted(() => {
       getProfile();
+      startTimer();
     });
 
     return {
-      displayMessage, // displayMessage を返す
+      displayMessage,
+      closedMessage,
       liffState,
       isInClient,
     };
